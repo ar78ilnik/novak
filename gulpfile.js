1e622147e10b60b958eb56db124277cd7ff7c44a
ar78ilnik <arakchiev.ilya@yandex.ru>
2022-10-27 22:31:54 +0300

    css task

--- gulpfile.js
+++ gulpfile.js
@@ -20,13 +20,17 @@ import {
 import {
     server
 } from "./gulp/tasks/server.js";
+import {
+    scss
+} from "./gulp/tasks/scss.js";
 
 function watcher() {
     gulp.watch(path.watch.files, copy);
     gulp.watch(path.watch.html, html);
+    gulp.watch(path.watch.scss, scss);
 }
 
-const mainTasks = gulp.parallel(copy, html);
+const mainTasks = gulp.parallel(copy, html, scss);
 
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
