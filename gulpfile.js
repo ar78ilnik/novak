366e5dfe897799e32537ab57b24685e439e53181
ar78ilnik <arakchiev.ilya@yandex.ru>
2022-10-27 16:03:04 +0300

    HTML task

--- gulpfile.js
+++ gulpfile.js
@@ -14,11 +14,17 @@ import {
 import {
     reset
 } from "./gulp/tasks/reset.js";
+import {
+    html
+} from "./gulp/tasks/html.js";
 
 function watcher() {
     gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html);
 }
 
-const dev = gulp.series(reset, copy, watcher);
+const mainTasks = gulp.parallel(copy, html);
+
+const dev = gulp.series(reset, mainTasks, watcher);
 
 gulp.task('default', dev);
