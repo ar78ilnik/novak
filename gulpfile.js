2b6a4f22a0f6ef238328bc359a6739587398b205
ar78ilnik <arakchiev.ilya@yandex.ru>
2022-10-27 15:51:44 +0300

    Reset task

--- gulpfile.js
+++ gulpfile.js
@@ -11,11 +11,14 @@ global.app = {
 import {
     copy
 } from "./gulp/tasks/copy.js";
+import {
+    reset
+} from "./gulp/tasks/reset.js";
 
 function watcher() {
     gulp.watch(path.watch.files, copy);
 }
 
-const dev = gulp.series(copy, watcher);
+const dev = gulp.series(reset, copy, watcher);
 
 gulp.task('default', dev);
