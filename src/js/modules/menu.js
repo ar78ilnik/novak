fadd99a79ec7514b142b7e417520bad6df254fea
ar78ilnik <arakchiev.ilya@yandex.ru>
2022-11-02 21:36:37 +0300

    Close menu

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -8,11 +8,12 @@ export const menu = () => {
         buttonMenu.setAttribute('aria-expanded', !expanded);
         buttonMenu.classList.toggle('button_menu--open');
         listMenu.classList.add('list_menu--open');
-        closeMenu.classList.toggle('list__cross--open');
+        closeMenu.classList.add('list__cross--open');
     });
 
     closeMenu.addEventListener('click', () => {
         listMenu.classList.remove('list_menu--open');
         buttonMenu.setAttribute('aria-expanded', !expanded);
+        closeMenu.classList.remove('list__cross--open');
     });
 }
