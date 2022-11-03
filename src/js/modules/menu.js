af54c6b5388fc33c743184c60df88aa4adcb324c
ar78ilnik <arakchiev.ilya@yandex.ru>
2022-11-03 10:17:37 +0300

    Header mobile

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -6,14 +6,16 @@ export const menu = () => {
     buttonMenu.addEventListener('click', () => {
         let expanded = buttonMenu.getAttribute('aria-expanded') === 'true' || false;
         buttonMenu.setAttribute('aria-expanded', !expanded);
-        buttonMenu.classList.toggle('button_menu--open');
+        buttonMenu.classList.add('button_menu--open');
         listMenu.classList.add('list_menu--open');
         closeMenu.classList.add('list__cross--open');
     });
 
     closeMenu.addEventListener('click', () => {
-        listMenu.classList.remove('list_menu--open');
+        let expanded = buttonMenu.getAttribute('aria-expanded') === 'true' || false;
         buttonMenu.setAttribute('aria-expanded', !expanded);
+        buttonMenu.classList.remove('button_menu--open');
+        listMenu.classList.remove('list_menu--open');
         closeMenu.classList.remove('list__cross--open');
     });
 }
